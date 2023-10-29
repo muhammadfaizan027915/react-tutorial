267888b8ebb3a8c0dca66673b6748e6e87a05f4d
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2023-10-29 20:24:52 +0500

    [Done] Added form handling using states and object lierals.

--- src/components/registrationForm/index.js
+++ src/components/registrationForm/index.js
@@ -2,51 +2,102 @@ import { useEffect, useState } from "react";
 import "./styles.css";
 
 const RegistrationForm = () => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    console.log(name);
-  }, [name])
+// Without states
+// const formSubmit = (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.target)
 
-  const onNameChange = (event) => {
-    setName(event.target.value);
+//     const userInfo  = {
+//         name: formData.get('name'),
+//         phone: formData.get('phone'),
+//         email: formData.get('email'),
+//         password: formData.get('password')
+//     }
+
+//     console.log(userInfo)
+
+// }
+
+    // Method 1 using states
+//   const [name, setName] = useState("");
+//   const [phone, setPhone] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+
+//   const onNameChange = (event) => {
+//     setName(event.target.value);
+//   };
+
+//   const onPhoneChange = (event) => {
+//     setPhone(event.target.value);
+//   };
+
+//   const onEmailChange = (event) => {
+//     setEmail(event.target.value);
+//   };
+
+//   const onPasswordChange = (event) => {
+//     if (event.target.value.length > 6) {
+//       setError("Password length should be a 6 charachers!");
+//     } else {
+//       setPassword(event.target.value);
+//       setError("");
+//     }
+//   };
+
+
+// Method 2 using states
+const [inputValues, setInputValues] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    password: ''
+});
+
+// const onInputValue = (event) =>  {
+//     const name = event.target.name;
+//     const value = event.target.value;
+//     setInputValues((prev) => {
+        
+//         if(name == 'name' ) {
+//             prev.name = value
+//         }  else if (name == 'phone') {
+//             prev.phone = value
+//         } else if (name == 'email') {
+//             prev.email = value
+//         } else {
+//             prev.password = value
+//         }
+
+//         const newInputValue = prev
+
+//         return newInputValue
+//     })
+// }
+
+
+// Method 3 using states
+const onInputValue = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setInputValues(prev => {
+        const newValue = {...prev, [name]: value }
+        return newValue;
+    })
+
+}
+
+
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+    console.log(inputValues)
   };
-
-  const onPhoneChange = (event) => {
-    console.log(event.target.value);
-    setPhone(event.target.value);
-  };
-
-  const onEmailChange = (event) => {
-    console.log(event.target.value);
-    setEmail(event.target.value);
-  };
-
-  const onPasswordChange = (event) => {
-    console.log(event.target.value);
-    setPassword(event.target.value);
-  };
-
-  // const formSubmit = (event) => {
-  //     event.preventDefault();
-  //     const formData = new FormData(event.target)
-
-  //     const userInfo  = {
-  //         name: formData.get('name'),
-  //         phone: formData.get('phone'),
-  //         email: formData.get('email'),
-  //         password: formData.get('password')
-  //     }
-
-  //     console.log(userInfo)
-
-  // }
 
   return (
-    <form className="formContainer">
+    <form className="formContainer" onSubmit={onSubmitForm}  >
       <h2>Sign up</h2>
       <label className="fieldContainer">
         Name:
@@ -54,7 +105,7 @@ const RegistrationForm = () => {
           type="text"
           name="name"
           className="inputField"
-          onChange={onNameChange}
+          onChange={onInputValue}
         />
       </label>
 
@@ -64,7 +115,7 @@ const RegistrationForm = () => {
           type="tel"
           name="phone"
           className="inputField"
-          onChange={onPhoneChange}
+          onChange={onInputValue}
         />
       </label>
 
@@ -74,7 +125,7 @@ const RegistrationForm = () => {
           type="email"
           name="email"
           className="inputField"
-          onChange={onEmailChange}
+          onChange={onInputValue}
         />
       </label>
 
@@ -84,11 +135,11 @@ const RegistrationForm = () => {
           type="password"
           name="password"
           className="inputField"
-          onChange={onPasswordChange}
+          onChange={onInputValue}
         />
       </label>
 
-      <button type="submit">Sign up</button>
+      <button type="submit" >Sign up</button>
     </form>
   );
 };
